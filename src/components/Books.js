e40fd43f4946dbcf29e734c37f1e6f995d8db24e
Arif <marifullaah@gmail.com>
2022-10-06 20:24:18 +0600

    all books added

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,28 @@
-import React from 'react'
+
+
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+import Book from './Book'
 
 const Books = () => {
+    // before the learn React Router Dom we use to do data fetch we used useEffect now we use useLoaderHook
+
+    // const [books, setBooks] = useState([])
+    // useEffect(() => {
+    //   fetch('https://api.itbook.store/1.0/new')
+    //     .then(res => res.json())
+    //     .then(data => console.log(data))
+    // }, [])
+    const loaderData = useLoaderData()
+    const booksData = loaderData.books
+
     return (
-        <div>
-            <h1>hello i from book component</h1>
+        <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
+            <div className='grid gap-6 row-gap-5 mb-8 lg:grid-cols-4 sm:row-gap-6 sm:grid-cols-2'>
+                {booksData.map(book => (
+                    <Book key={book.isbn13} book={book} />
+                ))}
+            </div>
         </div>
     )
 }
